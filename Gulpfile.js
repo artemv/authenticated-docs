a457e3ff3b9c63fd889171d00a32cb4b7e9df3de
Artem Vasiliev <artem.job@gmail.com>
2017-03-30 22:35:32 +0300

    fix: copy all the assets
    for e.g. gitbook support

--- Gulpfile.js
+++ Gulpfile.js
@@ -9,18 +9,8 @@
 
   gulp.task('assets', [], () => {
     return gulp.src([
-        BASE_DIR + '/css/**',
-        BASE_DIR + '/js/**',
-        BASE_DIR + '/mkdocs/**',
-        BASE_DIR + '/fonts/**',
-        BASE_DIR + '/img/**',
-        BASE_DIR + '/images/**',
-        BASE_DIR + '/**/*.js',
-        BASE_DIR + '/**/*.css',
-        BASE_DIR + '/**/*.dll',
-        BASE_DIR + '/**/*.xml',
-        BASE_DIR + '/**/*.war',
-        BASE_DIR + '/**/*.png'
+      BASE_DIR + '/**/*',
+      '!**/*.html'
     ], { base: BASE_DIR })
       .pipe(debug())
       .pipe(gulp.dest(TARGET_DIR + '/public'));
